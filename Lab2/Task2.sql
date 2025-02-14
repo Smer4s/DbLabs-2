bc3c0762c543eec3b6454ddf05d2b8fe73931516
Smer4s <studnikita@gmail.com>
2025-02-14 16:50:52 +0300

    implemented task2

---
+++ Lab2/Task2.sql
@@ -0,0 +1,121 @@
+-- STUDENTS
+
+create sequence students_seq start with 1 increment by 1;
+
+create or replace trigger students_auto_increment before
+   insert on students
+   for each row
+begin
+   if :new.id is null then
+      select students_seq.nextval
+        into :new.id
+        from dual;
+   end if;
+end;
+/
+
+create or replace trigger students_check_unique_id before
+   insert or update on students
+   for each row
+declare
+   v_count number;
+begin
+   select count(*)
+     into v_count
+     from students
+    where id = :new.id;
+   if v_count > 0 then
+      raise_application_error(
+         -20001,
+         'ID уже существует в таблице STUDENTS'
+      );
+   end if;
+end;
+/
+
+drop trigger students_auto_increment;
+
+drop trigger students_check_unique_id;
+
+-- GROUPS
+
+create sequence groups_seq start with 1 increment by 1 nocache;
+
+create or replace trigger groups_auto_increment before
+   insert on groups
+   for each row
+begin
+   if :new.id is null then
+      select groups_seq.nextval
+        into :new.id
+        from dual;
+   end if;
+end;
+/
+
+create or replace trigger groups_check_unique_id before
+   insert or update on groups
+   for each row
+declare
+   v_count number;
+begin
+   select count(*)
+     into v_count
+     from groups
+    where id = :new.id;
+   if v_count > 0 then
+      raise_application_error(
+         -20002,
+         'ID уже существует в таблице GROUPS'
+      );
+   end if;
+end;
+/
+
+create or replace trigger groups_check_unique_name before
+   insert or update on groups
+   for each row
+declare
+   v_count number;
+begin
+   select count(*)
+     into v_count
+     from groups
+    where name = :new.name;
+   if v_count > 0 then
+      raise_application_error(
+         -20003,
+         'Название группы уже существует в таблице GROUPS'
+      );
+   end if;
+end;
+/
+
+drop trigger groups_auto_increment;
+
+drop trigger groups_check_unique_id;
+
+drop trigger groups_check_unique_name;
+
+-- TESTING FIELD
+
+insert into groups (
+   name,
+   c_val
+) values ( '253504',
+           0 );
+
+select *
+  from groups;
+
+insert into students (
+   name,
+   group_id
+) values ( 'NikitaStud',
+           1 );
+
+select *
+  from students
+ where name = 'NikitaStud';
+
+commit;
